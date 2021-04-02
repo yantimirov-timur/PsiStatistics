8613f9122009be199af2d3e84630735ca8bb2bda
yantimirov-timur <tyantimirov@yandex.ru>
2021-04-02 13:07:54 +0300

    Completed stats collector

--- src/main/kotlin/PsiStatistics.kt
+++ src/main/kotlin/PsiStatistics.kt
@@ -1,38 +1,25 @@
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import com.intellij.psi.impl.source.tree.LeafPsiElement
+import java.io.IOException
 
 class PsiStatistics : AnAction() {
     /**
-     * -Рассмотреть случай когда уже есть файл
-     * -Разбить на функции
-     * -Изменить request
+     * Написать тесты
      */
 
-    override fun actionPerformed(event: AnActionEvent) {
-        val psiFile = event.getData(CommonDataKeys.PSI_FILE)
-        val psiFilePath = event.getData(CommonDataKeys.VIRTUAL_FILE)?.parent
-        val statFile = psiFilePath?.createChildData("da", "${psiFile?.name}_PsiStat") //change requestor
-        Messages.showMessageDialog("File successful created", "Stats", Messages.getInformationIcon())
-
-        val psiLeafElements = mutableListOf<PsiElement>()
-
-        //Сбор PSI элементов
-        psiFile?.accept(object : PsiRecursiveElementWalkingVisitor() {
-            override fun visitElement(element: PsiElement) {
-                super.visitElement(element)
-                if (element is LeafPsiElement) {
-                    psiLeafElements.add(element)
-                }
-            }
-        })
-        //Подсчет статистики
+    /**
+     * Метод подсчета элементов
+     */
+    private fun countStats(list: List<PsiElement>): MutableMap<String, Int> {
         val mapLeafString = mutableMapOf<String, Int>()
-        for (elem in psiLeafElements) {
+        for (elem in list) {
             val stringElement = elem.toString()
             if (stringElement in mapLeafString) {
                 mapLeafString[stringElement] = mapLeafString[stringElement]!! + 1
@@ -40,9 +27,50 @@ class PsiStatistics : AnAction() {
                 mapLeafString[stringElement] = 1
             }
         }
-        //Заполнение файла
-        var infoStatistics = ""
-        mapLeafString.forEach { (psiElement, number) -> infoStatistics += "$psiElement - $number\n" }
-        statFile?.setBinaryContent(infoStatistics.toByteArray())
+        return mapLeafString
+    }
+
+    /**
+     * Главный метод
+     */
+    override fun actionPerformed(event: AnActionEvent) {
+        val psiFile = event.getData(CommonDataKeys.PSI_FILE)
+        val psiFilePath = event.getData(CommonDataKeys.VIRTUAL_FILE)?.parent
+        var statFile: VirtualFile? = null
+
+        if (psiFile == null || psiFile.language.id != "kotlin") {
+            Messages.showMessageDialog("Plugin work with .kt files", "Error", Messages.getErrorIcon())
+        } else {
+            try {
+                runWriteAction {
+                    statFile = psiFilePath?.createChildData("stat", "${psiFile.name}_PsiStat")
+                }
+                Messages.showMessageDialog("File successful created", "Error", Messages.getInformationIcon())
+                val psiLeafElements = mutableListOf<LeafPsiElement>()
+
+                //Сбор PSI элементов
+                psiFile.accept(object : PsiRecursiveElementWalkingVisitor() {
+                    override fun visitElement(element: PsiElement) {
+                        super.visitElement(element)
+                        if (element is LeafPsiElement) {
+                            psiLeafElements.add(element)
+                        }
+                    }
+                })
+                //подсчет статистики
+                val mapLeafString = countStats(psiLeafElements)
+
+                //Заполнение файла
+                var infoStatistics = ""
+                mapLeafString.forEach { (psiElement, number) -> infoStatistics += "$psiElement - $number\n" }
+
+                runWriteAction {
+                    statFile?.setBinaryContent(infoStatistics.toByteArray())
+                }
+            }
+            catch (ex: IOException) {
+                Messages.showMessageDialog("File already exists", "Error", Messages.getErrorIcon())
+            }
+        }
     }
 }
---
+++ src/test/kotlin/PsiStatisticsTest.kt
@@ -0,0 +1,31 @@
+import com.intellij.build.output.KotlincOutputParser
+import com.intellij.configurationStore.getOrCreateVirtualFile
+import com.intellij.ide.SelectInManager.getProject
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectBundle
+import com.intellij.openapi.project.getProjectDataPathRoot
+import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFileFactory
+import com.intellij.testFramework.ParsingTestCase
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
+import com.intellij.testFramework.fixtures.TestFixtureBuilder
+
+import org.junit.jupiter.api.Test
+import com.intellij.testFramework.fixtures.JavaTestFixtureFactory
+
+import com.intellij.testFramework.builders.JavaModuleFixtureBuilder
+
+
+internal class PsiStatisticsTest{
+
+    @Test
+    fun test(){
+
+
+    }
+
+}
