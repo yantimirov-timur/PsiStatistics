7c175b70bc538e76071efb0711434f4f9e752109
yantimirov-timur <tyantimirov@yandex.ru>
2021-04-08 12:00:52 +0300

    change vis

--- visualisator/graphic_build.py
+++ visualisator/graphic_build.py
@@ -22,12 +22,12 @@ def build_graphics(f):
     for key in all_statistics:
         numbers.append(key)
 
-    fig, ax = plt.subplots(figsize=(13, 7))
+    fig, ax = plt.subplots(figsize=(14.5, len(elements) / 2))
     ax.set_yticks(range(len(numbers)))
     ax.set_yticklabels(numbers, rotation=45)
     for i in range(len(all_statistics)):
         ax.hlines(i, 0, elements[i], colors='b')
-    fig.savefig('PsiStatistic.png')
+    fig.savefig('PsiStatistic' + f + '.png')
     plt.close(fig)
 
 
